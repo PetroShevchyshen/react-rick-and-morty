d9cc1c47667e5a6561b2444b3d38a63e118c282f
Petro <Petro.Shevchyshen@fivesysdev.com>
2024-08-01 17:43:15 +0300

    Created main page, created header

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
-import './App.scss'
+import React from "react";
+import "./App.scss";
+import Main from "./components/Main";
 
 function App() {
-
   return (
-    <>
-    </>
-  )
+    <React.Fragment>
+      <Main />
+    </React.Fragment>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Header/index.tsx
@@ -0,0 +1,20 @@
+import styles from "./Header.module.scss";
+
+function Header() {
+  return (
+    <header className={styles.header}>
+      <div className={styles.logo}>
+        <img src="../img/logo-rick-and-morty.png" alt="logo" />
+      </div>
+      <div className={styles.navigation}>
+        <ul>
+          <li>Characters</li>
+          <li>Episodes</li>
+          <li>Locations</li>
+        </ul>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
