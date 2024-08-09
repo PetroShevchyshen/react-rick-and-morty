774643c08f24c72ffc2ba512f21c8cf9a47c082e
Petro <Petro.Shevchyshen@fivesysdev.com>
2024-08-09 16:41:15 +0300

    Added search logic to characters section

--- src/components/Search/index.tsx
+++ src/components/Search/index.tsx
@@ -1,8 +1,19 @@
-import { FC } from "react";
+import { FC, useState } from "react";
 import { SearchProps } from "../../utilities/Interfaces/SearchProps";
 import styles from "./Search.module.scss";
 
-const Search: FC<SearchProps> = ({ title, placeholder }) => {
+const Search: FC<SearchProps> = ({ title, placeholder, searchFunction }) => {
+  const [inputValue, setInputValue] = useState("");
+
+  const onInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setInputValue(event.target.value);
+  };
+
+  const onSearch = () => {
+    searchFunction(inputValue);
+    setInputValue("");
+  };
+
   return (
     <div className={styles.search}>
       <label htmlFor="searchInput" className={styles.title}>
@@ -12,9 +23,13 @@ const Search: FC<SearchProps> = ({ title, placeholder }) => {
         className={styles.input}
         id="searchInput"
         type="text"
+        value={inputValue}
         placeholder={placeholder}
+        onChange={onInputChange}
       />
-      <button className={styles.searchBtn}>Search</button>
+      <button onClick={() => onSearch()} className={styles.searchBtn}>
+        Search
+      </button>
     </div>
   );
 };
--- src/utilities/Interfaces/SearchProps.ts
+++ src/utilities/Interfaces/SearchProps.ts
@@ -1,4 +1,5 @@
 export interface SearchProps {
   title: string;
   placeholder: string;
+  searchFunction: Function;
 }
